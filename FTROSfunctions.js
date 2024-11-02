63d4022a743d66bc53d0cea0d8f0a4ba57509159
Kfafa2024 <kfafa21@gmail.com>
2024-11-02 05:53:22 -0400

    Add files via upload

---
+++ FTROSfunctions.js
@@ -0,0 +1,8 @@
+let FTROS = Math.floor(Math.random () * 2) + 3;
+console.log(FTROS);
+
+function getRandomNumber(low, high) {
+let Play = Math.floor(Math.random() * (high - low +1)) + low;
+return Play
+
+} 
